cbb6e760f8b4d424632101461f7edff493a6a4fe
0ri0n09 <jonathan.jaremczuk@ynov.com>
2023-05-09 17:02:07 +0200

    repair fridge

--- src/app/pages/food/food.page.ts
+++ src/app/pages/food/food.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore, deleteDoc, doc, getDocs, collection } from '@angular/fire/firestore';
+import {Firestore, deleteDoc, doc, getDocs, collection, addDoc} from '@angular/fire/firestore';
 import { AuthService } from '../../services/auth.service';
 import { ProfileService } from '../profile/profile.service';
 import { catchError, map } from 'rxjs/operators';
 import { EMPTY } from 'rxjs';
 import { User } from '@angular/fire/auth';
+import { SpoonacularService } from "../../services/spoonacular.service";
 
 @Component({
   selector: 'app-food',
@@ -13,12 +14,16 @@ import { User } from '@angular/fire/auth';
 })
 export class FoodPage implements OnInit {
   ingredients: any[];
+  searchQuery: string;
+  searchResults: any[];
   private currentUser: User;
 
   constructor(
       private firestore: Firestore,
       private authService: AuthService,
-      private profileService: ProfileService
+      private profileService: ProfileService,
+
+      private spoonacularService: SpoonacularService
   ) {}
 
   ngOnInit() {
@@ -61,7 +66,41 @@ export class FoodPage implements OnInit {
         );
   }
 
-  async deleteIngredient(ingredientId: string) {
+    selectIngredients() {
+        this.spoonacularService.searchIngredients(this.searchQuery).subscribe(
+            (response: any) => {
+                this.searchResults = response.results;
+                this.searchResults.forEach((searchResult, index) => {
+                    const foundIngredient = this.ingredients.find((ingredient) => ingredient.name === searchResult.name);
+                    if (foundIngredient) {
+                        this.searchResults.splice(index, 1);
+                    }
+                });
+            },
+            (error) => {
+                console.log('Error searching ingredients:', error);
+            }
+        );
+    }
+
+    addIngredient(ingredientId: string, ingredientName: string, ingredientImage: string) {
+        const ingredient = {
+            id: ingredientId,
+            name: ingredientName,
+            image: 'https://spoonacular.com/cdn/ingredients_100x100/' + ingredientImage
+        };
+        const ingredientsCollection = collection(
+            this.firestore,
+            `users/${this.currentUser.uid}/fridges/fridge/ingredients`
+        );
+        addDoc(ingredientsCollection, ingredient).then(() => {
+            this.loadIngredients();
+        }).catch((error) => {
+            console.log('Error adding ingredient:', error);
+        });
+    }
+
+    async deleteIngredient(ingredientId: string) {
     const ingredientReference = doc(
         this.firestore,
         `users/${this.currentUser.uid}/fridges/fridge/ingredients/${ingredientId}`
